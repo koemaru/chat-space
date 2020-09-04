f68135c226b233530cc25e317ab70eeb104ce32f
koemaru <yuhaya.1027@gmail.com>
2020-09-04 21:49:52 +0900

    Update message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,45 @@
-$(function() {
+$(function(){
+  function buildHTML(message){
+    if ( message.image ) {
+      let html = 
+      `<div class="messages">
+        <div class="messages__box">
+          <p class="messages__box__username">
+            ${message.user_name}
+          </p>
+          <p class="messages__box__date">
+            ${message.created_at}
+          </p>
+        </div>
+      <div class="message">
+        <p class="message__content">
+          ${message.content}
+        </p>
+        <img class="message__image" src="${message.image_url}">
+      </div>
+    </div>`
+    return html;
+    } else {
+      let html =
+      `<div class="messages">
+        <div class="messages__box">
+          <p class="messages__box__username">
+            ${message.user_name}
+          </p>
+          <p class="messages__box__date">
+            ${message.created_at}
+          </p>
+        </div>
+        <div class="message">
+          <p class="message__content">
+            ${message.content}
+          </p>
+        </div>
+      </div>`
+      return html;
+    };
+  }
+
   $('.new-message').on('submit', function(e){
     e.preventDefault();
     let formData = new FormData(this);
@@ -10,6 +51,16 @@ $(function() {
       dataType: 'json',
       processData: false,
       contentType: false
+    })
+    .done(function(data){
+      let html = buildHTML(data);
+      $('.main-messages').append(html);
+      $('.main-messages').animate({ scrollTop: $('.main-messages')[0].scrollHeight});
+      $('.submit-btn').prop("disabled", false);
+      $('.new-message')[0].reset();
+    })
+    .fail(function(){
+      alert("メッセージ送信に失敗しました");
     })
   });
 });
